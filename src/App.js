2e0202f57eee55dfb86572e16a75044bd1a12ab8
imtiazpy <ahmed.imti1994@gmail.com>
2021-08-09 22:59:38 +0600

    character randomization done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+// import Item from './MyItem';
+import React from 'react';
+
+
+class StarWars extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      homeworld: null,
+      films: []
+    }
+  }
+
+
+  getNewCharacter() {
+    const randomNum = Math.round(Math.random() * 82)
+    const url = `https://swapi.dev/api/people/${randomNum}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          films: data.films,
+          loadedCharacter: true
+        })
+      })
+
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.loadedCharacter &&
+          <div>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.height}</p>
+            <p><a href={this.state.homeworld}>Homeworld</a></p>
+            <ul>
+              <li>{this.state.films}</li>
+            </ul>
+          </div>
+        }
+
+        <button type="button" onClick={() => this.getNewCharacter()} className="btn">Randomize Characters</button>
+      </div>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
 }
 
+
+
 export default App;
+
---
+++ src/MyItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+class Item extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clicks: 0,
+            totalRemaining: 100
+        }
+    }
+
+    clickMe() {
+        this.setState({
+            clicks: this.state.clicks + 1,
+            totalRemaining: this.state.totalRemaining - 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 onClick={() => this.clickMe()}>Hello from {this.props.name}</h1>
+                <span>Click {this.state.clicks} of {this.state.totalRemaining}</span>
+            </div>
+        )
+    }
+}
+
+export default Item;
